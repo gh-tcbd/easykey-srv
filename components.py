ebf4ceba80cc636637a269219ad2d5ae646216a6
scensorECHO <xxartofwarxx@gmail.com>
2016-03-19 17:49:52 -0400

    Adding component devices template

---
+++ components.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+class APIProduct:
+    def __init__(self):
+        self.api_key = ""
+        self.email = ""
+        self.password = ""
+        print()
+        
+        
+class NestDevice(APIProduct):
+    def __init__(self):
+        super()
+        
+class WemoDevice(APIProduct):
+    def __init__(self):
+        super()
+        
+class HueDevice(APIProduct):
+    def __init__(self):
+        super()
+        
+class LinkitronDevice(APIProduct):
+    def __init__(self):
+        super()
