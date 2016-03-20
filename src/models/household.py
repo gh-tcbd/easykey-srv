b3e0cc9aac63d0651168632feb0da7b0bf8ebb3a
chulderman <chase@hulderman.com>
2016-03-20 02:53:07 -0400

    Added mutators

--- src/models/household.py
+++ src/models/household.py
@@ -12,11 +12,30 @@ class Household():
         		# otherwise continue.
         		for user, auth_level in users:
         			if validate_email(user):
-        				users[user] = auth_level
+        				self.users[user] = auth_level
         	if not services:
         		self.services = {"wemo": False, "hue": False, "lockitron": False, "nest": False}
             return True
         return False
+    def set_name(self, name=""):
+    	if validate_name(name):
+    		self.name = name
+    		return True
+    	else:
+    		return False
+    def set_user(self, user_id=""):
+    	if not user_id in self.users:
+    		self.users[user_id] = 0
+    def set_auth(self, user_id="", auth_level=0):
+    	if user_id in self.users:
+    		self.users[user_id] = auth_level
+    		return True
+    	return False
+    def set_service(self, service="", enable=False):
+    	if service in self.services:
+    		self.services[service]=enable
+    		return True
+    	return False
     def get_name(self):
     	try:
     		return self.name
